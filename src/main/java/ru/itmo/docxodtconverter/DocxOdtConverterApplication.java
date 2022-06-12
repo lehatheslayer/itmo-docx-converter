24450cff5c60fe9d385ad5911cc06582d409514f
egorovm <egorov_michil@mail.ru>
2022-06-12 23:44:04 +0300

    Spring project init

---
+++ src/main/java/ru/itmo/docxodtconverter/DocxOdtConverterApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.docxodtconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocxOdtConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocxOdtConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/itmo/docxodtconverter/DocxOdtConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.docxodtconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocxOdtConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
